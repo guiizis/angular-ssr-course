9392da095f349649c45ae3e69307d3955a340c47
guiziis <guigamoda@hotmail.com>
2025-10-08 13:38:01 -0300

    prerender config

--- angular-ssr-course-1-start/src/app/app.routes.server.ts
+++ angular-ssr-course-1-start/src/app/app.routes.server.ts
@@ -1,9 +1,14 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, ServerRoute, PrerenderFallback } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'courses/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+
+    async getPrerenderParams() {
+      return [{ id: '01' }];
+    }
   },
   {
     path: '**',
