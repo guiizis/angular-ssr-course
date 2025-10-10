5909ca144b2d484b2f6c6dd72ae27cbd2b48a326
guiziis <guigamoda@hotmail.com>
2025-10-10 13:35:45 -0300

    add logic to diractive

--- angular-ssr-course-1-start/src/directives/app-shell-render.directive.ts
+++ angular-ssr-course-1-start/src/directives/app-shell-render.directive.ts
@@ -1,15 +1,21 @@
 import { isPlatformServer } from "@angular/common";
-import { Directive, ElementRef, Inject, PLATFORM_ID } from "@angular/core";
+import { Directive, Inject, OnInit, PLATFORM_ID, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
   selector: '[appShellRender]'
 })
-export class AppShellRenderDirective {
+export class AppShellRenderDirective implements OnInit {
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
-    private el: ElementRef) {
-    if (isPlatformServer(this.platformId)) {
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef) {
+  }
 
+  ngOnInit(): void {
+    if (isPlatformServer(this.platformId)) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+      return;
     }
+    this.viewContainerRef.clear();
   }
 }
